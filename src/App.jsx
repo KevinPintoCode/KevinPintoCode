82767050e8c48f09582454a2c4699208be64c346
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-06-01 15:24:44 +0200

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="projects" element={<Portfolio />} />
