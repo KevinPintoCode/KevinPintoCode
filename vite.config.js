70ce9d2bdaebc9e0ca8fb2793975d73cf38e1715
Kevin "Goss" Pinto <kevinpintocode@gmail.com>
2024-06-01 16:04:43 +0200

    build changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: "_redirects", dest: "dist" },
+        // Add more targets if needed
+      ],
+      hook: "writeBundle", // Run after bundle is written
+    }),
+  ],
 });
